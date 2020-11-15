7e2e2262b304fefefb135436c6cccae68b8e38dd
sam <sam@freighttrust.com>
2020-11-15 06:29:59 -0800

    feat(inline): inline the function

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 // SPDX-License-Identifier: ISC
-'use strict';
-
-import "bootswatch/dist/spacelab/bootstrap.min.css";
-
+"use strict";
 function toGithubRenderURL() {
-	var input = document.getElementsByName("input")[0].value;
-	var output =
-		'<img src="https://render.githubusercontent.com/render/math?math=' +
-		encodeURIComponent(input) +
-		'">';
-	document.getElementsByName("output")[0].value = output;
-	document.getElementById("result").innerHTML = output;
+  var input = document.getElementsByName("input")[0].value;
+  var output =
+    '<img src="https://render.githubusercontent.com/render/math?math=' +
+    encodeURIComponent(input) +
+    '">';
+  document.getElementsByName("output")[0].value = output;
+  document.getElementById("result").innerHTML = output;
 }
-
